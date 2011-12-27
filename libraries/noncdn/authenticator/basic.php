1c9a21190175dbca28bc3913daa921bb5567b640
Sam Moffatt <pasamio@gmail.com>
2011-12-26 16:17:16 -0800

    Docblock updates

--- libraries/noncdn/authenticator/basic.php
+++ libraries/noncdn/authenticator/basic.php
@@ -2,17 +2,49 @@
 
 namespace NonCDN;
 
+/**
+ * Basic Challenge authenticator
+ *
+ * @package     NonCDN
+ * @subpackage  Authenticator
+ */
 class Authenticator_Basic
 {
+	/** 
+	 * @var    object  Configuration
+	 * @since  1.0
+	 */
 	protected $configuration;
+	
+	/**
+	 * @var    object  Factory
+	 * @since  1.0
+	 */
 	protected $factory;
 	
+	/**
+	 * Constructor
+	 *
+	 * @param   object  $config   Configuration object
+	 * @param   object  $factory  Factory
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
 	public function __construct($config, $factory)
 	{
 		$this->configuration = $config;
 		$this->factory = $factory;
 	}	
 	
+	/**
+	 * Authenticate a request
+	 *
+	 * @return  string  username of the requestor.
+	 *
+	 * @since   1.0
+	 */
 	public function authenticate()
 	{
 		if (!isset($_SERVER['PHP_AUTH_USER'])) {
@@ -29,7 +61,14 @@ class Authenticator_Basic
 		return $_SERVER['PHP_AUTH_USER'];
 	}
 	
-	public function requestAuth()
+	/**
+	 * Attempt to authenticate the user
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	protected function requestAuth()
 	{
 		$realm = $this->configuration->getRealm();		
 		header('WWW-Authenticate: Basic realm="'. $realm .'"');
--- libraries/noncdn/client/authnode.php
+++ libraries/noncdn/client/authnode.php
@@ -2,14 +2,42 @@
 
 namespace NonCDN;
 
+/**
+ * Client for the auth node
+ *
+ * @package     NonCDN
+ * @subpackage  Client
+ */
 class Client_AuthNode
 {
+	/**
+	 * @var    object  configuration class
+	 * @since  1.0
+	 */
 	private $configuration;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param   object  $configuration  Configuration object.
+	 *
+	 * @return  void
+	 */
 	public function __construct($configuration)
 	{
 		$this->configuration = $configuration;
 	}	
 	
+	/**
+	 * Validate credentials for a username and token.
+	 *
+	 * @param   string  $username  Username of the requestor.
+	 * @param   string  $token     An authentication token for the request.
+	 *
+	 * @return  boolean  If the credentials are valid or not.
+	 *
+	 * @since   1.0
+	 */	 
 	public function validate_credentials($username, $token)
 	{
 		$server = $this->configuration->getAuthServer();
