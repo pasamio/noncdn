b7eb5afbfddcef5d21db9f06f1c4b2ec5eb6f53b
Sam Moffatt <pasamio@gmail.com>
2011-12-26 16:12:57 -0800

    Added edge map to master configuration; minor style updates

--- master/configuration.php
+++ master/configuration.php
@@ -2,12 +2,17 @@
 
 class NonCDNConfiguration
 {
-	var $appname = '\NonCDN\Master';
-	var $auth_servers = Array('http://localhost/~pasamio/usq/noncdn/auth/');
-	var $edge_servers = Array('http://localhost/~pasamio/usq/noncdn/edge/');
-	var $edge_secrets = Array('usq');
-	var $master_servers = Array('http://localhost/~pasamio/usq/master/');
-	var $max_token_age = 12; // hours
-	var $authenticator = '\NonCDN\Authenticator_Basic';
+	public $appname = '\NonCDN\Master';
+	public $auth_servers = array('http://localhost/~pasamio/usq/noncdn/auth/');
+	public $edge_servers = array('http://localhost/~pasamio/usq/noncdn/edge/');
+	public $edge_secrets = array('usq');
+	public $master_servers = array('http://localhost/~pasamio/usq/master/');
+	public $max_token_age = 12; // hours
+	public $authenticator = '\NonCDN\Authenticator_Basic';
+	
+	public $edge_map = array(
+			'::1' => array(0),
+			'127.0.0.1/8' => array(0)
+		);
 }
 
