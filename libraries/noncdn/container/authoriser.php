44eb8c7595e8bb76e72dbad8e745238fc8db53af
Sam Moffatt <pasamio@gmail.com>
2011-12-26 16:14:09 -0800

    Expand out authoriser code to work properly

--- libraries/noncdn/container/authoriser.php
+++ libraries/noncdn/container/authoriser.php
@@ -19,8 +19,18 @@ class Container_Authoriser
 		
 		$response = file_get_contents($server.'container/check_user_access?'.$data);
 		
-		var_dump($response);
+		$data = json_decode($response);
 		
-		return true;
+		if (isset($data->error) && $data->error)
+		{
+			return false;
+		}
+		
+		if (isset($data->result))
+		{
+			return $data->result;
+		}
+		
+		return false;
 	}
 }
