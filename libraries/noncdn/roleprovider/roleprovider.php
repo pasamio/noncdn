8319390489537d354a2f89bc75a2a77bf5108515
Sam Moffatt <pasamio@gmail.com>
2012-01-09 01:06:24 -0800

    Added RoleProvider interface

---
+++ libraries/noncdn/roleprovider/roleprovider.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     NonCDN
+ * @subpackage  RoleProvider
+ * @copyright   Copyright (C) 2012 Sam Moffatt  
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+ 
+namespace NonCDN;
+ 
+ /**
+ * Role Provider interface.
+ *
+ * @package     NonCDN
+ * @subpackage  RoleProvider
+ * @since       1.0
+ */
+interface RoleProvider
+{
+	/**
+	 * Get roles from this provider.
+	 *
+	 * @param   string  $username  The username to retrieve roles from.
+	 *
+	 * @return  array  The list of roles for this user.
+	 *
+	 * @since   1.0
+	 */
+	public function getRoles($username)
+}
