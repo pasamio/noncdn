ee341af9b0068df06ecc02c1c265ae8f049e63f7
Sam Moffatt <pasamio@gmail.com>
2012-01-14 13:38:46 -0800

    Style improvements

--- cli.php
+++ cli.php
@@ -4,15 +4,15 @@ namespace NonCDN;
 defined('NONCDN') or die();
 
 // Load the Joomla! Platform
-define('JPATH_PLATFORM', __DIR__.'/libraries/joomla-platform/libraries');
+define('JPATH_PLATFORM', __DIR__ . '/libraries/joomla-platform/libraries');
 define('JPATH_ROOT', __DIR__);
 define('JPATH_BASE', __DIR__);
 define('DS', DIRECTORY_SEPARATOR);
 
 // Load and setup the Joomla! class loader plus backports and eBay content branch
-require __DIR__.'/libraries/import.php';
+require __DIR__ . '/libraries/import.php';
 
 // Load the PSR-0 Class Loader and setup NonCDN namespace
-require __DIR__.'/libraries/psrloader.php';
-$loader = new \SplClassLoader('NonCDN', __DIR__.'/libraries');
+require __DIR__ . '/libraries/psrloader.php';
+$loader = new \SplClassLoader('NonCDN', __DIR__ . '/libraries');
 $loader->register();
