c3b8aad3b6689212692a3cacaab2f7b182b6f2d1
Sam Moffatt <pasamio@gmail.com>
2012-01-21 15:38:21 -0800

    Expand RequestSupport class to return JSON and fix status codes

--- libraries/noncdn/requestsupport.php
+++ libraries/noncdn/requestsupport.php
@@ -4,13 +4,42 @@ namespace NonCDN;
 
 class RequestSupport
 {
-	static function terminate($errorcode, $statusline, $message = '')
+	static function terminate($errorcode, $statusline, $message = '', $format = 'html', $errorsubcode = 0)
 	{
-		if(empty($message))
+		// Apache Support
+		header('HTTP/1.0 ' . $errorcode . ' ' . $statusline);
+		
+		// FastCGI Support
+		header('Status: ' . $errorcode . ' ' . $statusline);
+		
+		$response = '';
+		
+		$error_array = array(
+							'error'=>true,
+							'errorcode' => $errorcode,
+							'errorsubcode' => $errorsubcode,
+							'statusline' => $statusline,
+							'message' => $message
+						);
+		
+		switch($format)
 		{
-			$message = $errorcode .' '. $statusline;
+			case 'json':
+				$response = json_encode($error_array);
+				break;
+			
+			case 'html':
+			default:
+				$response .= '<html><head><title>' . $statusline . '</title></head>';
+				$response .= '<body><h1>'. $errorcode . ' ' . $statusline . '</h1>';
+				
+				if (!empty($message))
+				{
+					$response .= '<hr /><p>'. $message .'</p>';
+				}
+				$response .= '</body></html>';
+				break;
 		}
-		header($errorcode .' '. $statusline);
-		die('<html><head><title>'. $message.'</title></head><body><h1>'. $message .'</h1></body></html>');
+		die($response);
 	}
 }
