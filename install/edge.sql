82dab7d3e78aecedd576cef2e843033b182d8079
Sam Moffatt <pasamio@gmail.com>
2012-01-21 00:21:15 -0800

    Filling out edge server cache DB schema

--- install/edge.sql
+++ install/edge.sql
@@ -15,3 +15,25 @@ use_count INTEGER,
 file_mime VARCHAR(100),
 last_access DATE
 );
+
+CREATE TABLE user_role
+(
+user_name VARCHAR(100),
+role_id INTEGER,
+PRIMARY KEY (user_name, role_id)
+);
+
+CREATE TABLE roles
+(
+role_id INTEGER PRIMARY KEY,
+role_name VARCHAR(100),
+admin_username VARCHAR(100)
+);
+
+CREATE TABLE container_role
+(
+container_name VARCHAR(100),
+role_id INTEGER,
+type VARCHAR(50),
+PRIMARY KEY (container_name, role_id)
+);
