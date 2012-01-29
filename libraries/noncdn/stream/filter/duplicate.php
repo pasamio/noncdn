662961fd686f2c847377a0fafe1ffc75df057bd3
Sam Moffatt <pasamio@gmail.com>
2012-01-28 21:12:36 -0800

    Code style fixes

--- libraries/noncdn/stream/filter/duplicate.php
+++ libraries/noncdn/stream/filter/duplicate.php
@@ -18,43 +18,61 @@ namespace NonCDN;
 class Stream_Filter_Duplicate extends \PHP_User_Filter
 {
 	/**
+	 * Filter the stream
 	 *
+	 * @param   resource  $in         The incoming bucket brigade.
+	 * @param   resource  $out        The outgoing bucket brigade.
+	 * @param   integer   &$consumed  The amount of data consumed.
+	 * @param   boolean   $closing    If the stream is closing.
+	 *
+	 * @return  integer  The status to return (we always pass on not ask for more)
+	 *
+	 * @since   1.0
 	 */
-	public function filter($in, $out, &$consumed, $closing) {
+	public function filter($in, $out, &$consumed, $closing)
+	{
+		while ($bucket = stream_bucket_make_writeable($in))
+		{
+			$result = fwrite($this->fh, $bucket->data);
+			if ($result != strlen($bucket->data))
+			{
+				throw new Exception('stream duplication error');
+			}
+			$consumed += $bucket->datalen;
+			stream_bucket_append($out, $bucket);
+		}
+		return PSFS_PASS_ON;
+	}
 
-        while ($bucket = stream_bucket_make_writeable($in)) {
-            $result = fwrite($this->fh, $bucket->data);
-            if($result != strlen($bucket->data))
-            {
-	            throw new Exception('stream duplication error');
-            }
-            $consumed += $bucket->datalen;
-            stream_bucket_append($out, $bucket);
-        }
-        return PSFS_PASS_ON;
-    }
-    
-    /**
-     * 
-     */
-    public function onCreate()
-    {
-    	if (isset($this->params['outFile']))
-    	{
-	    	$this->fh = fopen($this->params['outFile'], 'w');
-	    	return (bool) $this->fh;
-    	}
-    	else
-    	{
-	    	return false;
-    	}
-    }
-    
-    /**
-     * 
-     */
-    public function onClose()
-   	{
-	   	fclose($this->fh);
-   	}
+	/**
+	 * Create hook
+	 *
+	 * @return  boolean  If the open succeeded.
+	 *
+	 * @since   1.0
+	 */
+	public function onCreate()
+	{
+		if (isset($this->params['outFile']))
+		{
+			$this->fh = fopen($this->params['outFile'], 'w');
+			return (bool) $this->fh;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Close hook
+	 *
+	 * @return  void  Unknown.
+	 *
+	 * @since   1.0
+	 */
+	public function onClose()
+	{
+		fclose($this->fh);
+	}
 }
