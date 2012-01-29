064884a5c897431915ae7a42eeecc5e8f81ffbfb
Sam Moffatt <pasamio@gmail.com>
2012-01-28 18:30:49 -0800

    Adding interface and fixing the autoloader so it works properly

--- libraries/psrloader.php
+++ libraries/psrloader.php
@@ -1,3 +1,70 @@
+<?php
+
+/**
+ * SplAutoloader defines the contract that any OO based autoloader must follow.
+ *
+ * @author Guilherme Blanco <guilhermeblanco@php.net>
+ */
+interface SplAutoloader
+{
+    /**
+     * Defines autoloader to work silently if resource is not found.
+     *
+     * @const
+     */
+    const MODE_SILENT = 0;
+ 
+    /**
+     * Defines autoloader to work normally (requiring an un-existent resource).
+     *
+     * @const
+     */
+    const MODE_NORMAL = 1;
+ 
+    /**
+     * Defines autoloader to work in debug mode, loading file and validating requested resource.
+     *
+     * @const
+     */
+    const MODE_DEBUG = 2;
+ 
+    /**
+     * Define the autoloader work mode.
+     *
+     * @param integer $mode Autoloader work mode.
+     */
+    public function setMode($mode);
+ 
+    /**
+     * Add a new resource lookup path.
+     *
+     * @param string $resourceName Resource name, namespace or prefix.
+     * @param mixed $resourcePath Resource single path or multiple paths (array).
+     */
+    public function add($resourceName, $resourcePath = null);
+ 
+    /**
+     * Load a resource through provided resource name.
+     *
+     * @param string $resourceName Resource name.
+     */
+    public function load($resourceName);
+ 
+    /**
+     * Register this as an autoloader instance.
+     *
+     * @param boolean Whether to prepend the autoloader or not in autoloader's list.
+     */
+    public function register($prepend = false);
+ 
+    /**
+     * Unregister this autoloader instance.
+     *
+     */
+    public function unregister();
+}
+
+
 /**
  * SplClassLoader implementation that implements the technical interoperability
  * standards for PHP 5.3 namespaces and class names.
@@ -145,6 +212,11 @@ class SplClassLoader implements SplAutoloader
     {
         $resourceAbsolutePath = $this->getResourceAbsolutePath($resourceName);
  
+	if (empty($resourceAbsolutePath) || is_file($resourceAbsolutePath))
+	{
+		return;
+	}
+
         switch (true) {
             case ($this->mode & self::MODE_SILENT):
                 if ($resourceAbsolutePath !== false) {
@@ -217,7 +289,7 @@ class SplClassLoader implements SplAutoloader
             $resourcePath      =  str_replace('\\', DIRECTORY_SEPARATOR, $resourceNamespace) . DIRECTORY_SEPARATOR;
         }
  
-        return $resourcePath . str_replace('_', DIRECTORY_SEPARATOR . $resourceName) . $this->fileExtension;
+        return $resourcePath . str_replace('_', DIRECTORY_SEPARATOR, $resourceName) . $this->fileExtension;
     }
  
     /**
